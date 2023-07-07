b42ac60defca7a28bf992a1d469e353e65a301fc
Francine dos Santos <125395105+FranNinaa@users.noreply.github.com>
2023-07-07 19:20:22 -0300

    Update Settings.Designer.cs

--- AcessoBancoDados/Properties/Settings.Designer.cs
+++ AcessoBancoDados/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace AcessoBancoDados.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=FRAN-CINE\\SQLEXPRESS;Initial Catalog=Consultorio;Integrated Security=" +
+        [global::System.Configuration.DefaultSettingValueAttribute("\\SQLEXPRESS;Initial Catalog=Consultorio;Integrated Security=" +
             "True")]
         public string StringConexao {
             get {
